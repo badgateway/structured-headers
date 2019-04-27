5d823f79f4af9b8e7fe262409af86f2500ea4ab6
Evert Pot <me@evertpot.com>
2019-04-27 00:10:27 -0400

    Lint fixes

--- src/index.ts
+++ src/index.ts
@@ -4,72 +4,72 @@ import { Dictionary, Item, List, ListList, ParameterizedList } from './types';
 
 module.exports = {
 
-  parseDictionary: function(input: string): Dictionary {
+  parseDictionary: (input: string): Dictionary => {
 
-    let parser = new Parser(input);
+    const parser = new Parser(input);
     return parser.parseDictionary();
 
   },
 
-  parseList: function(input: string): List {
+  parseList: (input: string): List => {
 
-    let parser = new Parser(input);
+    const parser = new Parser(input);
     return parser.parseList();
 
   },
 
-  parseListList: function(input: string): ListList {
+  parseListList: (input: string): ListList => {
 
-    let parser = new Parser(input);
+    const parser = new Parser(input);
     return parser.parseListList();
 
   },
 
-  parseParamList: function(input: string): ParameterizedList {
+  parseParamList: (input: string): ParameterizedList => {
 
-    let parser = new Parser(input);
+    const parser = new Parser(input);
     return parser.parseParamList();
 
   },
 
-  parseItem: function(input: string): Item {
+  parseItem: (input: string): Item => {
 
-    let parser = new Parser(input);
+    const parser = new Parser(input);
     return parser.parseItem();
 
   },
 
-  serializeDictionary: function(input: Dictionary): string {
+  serializeDictionary: (input: Dictionary): string => {
 
-    let serializer = new Serializer();
+    const serializer = new Serializer();
     return serializer.serializeDictionary(input);
 
   },
 
-  serializeList: function(input: List) {
+  serializeList: (input: List) => {
 
-    let serializer = new Serializer();
+    const serializer = new Serializer();
     return serializer.serializeList(input);
 
   },
 
-  serializeListList: function(input: ListList) {
+  serializeListList: (input: ListList) => {
 
-    let serializer = new Serializer();
+    const serializer = new Serializer();
     return serializer.serializeListList(input);
 
   },
 
-  serializeParamList: function(input: ParameterizedList) {
+  serializeParamList: (input: ParameterizedList) => {
 
-    let serializer = new Serializer();
+    const serializer = new Serializer();
     return serializer.serializeParamList(input);
 
   },
 
-  serializeItem: function(input: Item) {
+  serializeItem: (input: Item) => {
 
-    let serializer = new Serializer();
+    const serializer = new Serializer();
     return serializer.serializeItem(input);
 
   },
